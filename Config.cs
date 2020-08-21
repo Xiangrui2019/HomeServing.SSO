ba24273d0f53c2866e820c8cb98fd758bb1cf035
HomeServing <xiangrui@aiursoft.com>
2020-08-21 14:41:12 +0800

    A very Small Changes

--- Config.cs
+++ Config.cs
@@ -14,11 +14,11 @@ namespace HomeServing.SSO
                    {
                         new IdentityResources.OpenId(),
                         new IdentityResources.Profile(),
-                        new IdentityResources.Address(),
-                        new IdentityResources.Phone(),
                         new IdentityResource("avatar", "头像", new List<string> { JwtClaimTypes.Picture }),
-                        new IdentityResource("profile", "个人简介", new List<string> { JwtClaimTypes.Profile }),
-                        new IdentityResource("nick_name", "昵称", new List<string> { JwtClaimTypes.NickName })
+                        new IdentityResource("nick_name", "昵称", new List<string> { JwtClaimTypes.NickName }),
+                        new IdentityResource("bio", "个人简介", new List<string> { JwtClaimTypes.Profile }),
+                        new IdentityResource("gender", "性别", new List<string> { JwtClaimTypes.Gender }),
+                        new IdentityResource("user_name", "用户名", new List<string> { JwtClaimTypes.PreferredUserName })
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
