b24162a4f03e163c4d20bb287576769533c9c427
HomeServing <xiangrui@aiursoft.com>
2020-08-22 12:22:19 +0800

    add basic role management pages.

--- Modules/Role/RoleController.cs
+++ Modules/Role/RoleController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using HomeServing.SSO.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeServing.SSO.Modules.Role
 {
     public class RoleController : Controller
     {
-        public RoleController()
-        {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
+        public RoleController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager
+                .Roles
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(roles);
         }
     }
 }
