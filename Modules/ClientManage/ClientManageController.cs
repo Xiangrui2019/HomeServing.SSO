58a1bd467b1b1d9ffdbb17fa27ad6e10d63c85d8
HomeServing <xiangrui@aiursoft.com>
2020-08-23 09:33:38 +0800

    Client Manage

--- Modules/ClientManage/ClientManageController.cs
+++ Modules/ClientManage/ClientManageController.cs
@@ -1,12 +1,22 @@
 ﻿using AutoMapper;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeServing.SSO.Modules.ClientManage
 {
     public class ClientManageController : Controller
     {
-        public ClientManageController()
+        private readonly ConfigurationDbContext _configurationDbContext;
+        private readonly IMapper _mapper;
+
+        public ClientManageController(
+            ConfigurationDbContext configurationDbContext,
+            IMapper mapper)
         {
+            _configurationDbContext = configurationDbContext;
+            _mapper = mapper;
         }
+
+
     }
 }
